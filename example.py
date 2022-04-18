29e9109eb360499268b9bd573abbcbb6d7864c6f
Henry Rivera <henryrivera.cs@gmail.com>
2022-04-18 11:33:37 -0400

    example added for noobs

---
+++ example.py
@@ -0,0 +1,11 @@
+from logger import Log
+
+my_log = Log(Name="My App")
+my_log.log_info("Hello world!")
+
+x = 20
+if x > 21:
+    my_log.log_ok("X is greater than 21")
+else:
+    my_log.log_error("The math is wrong")
+my_log.log_warning("Done.")
