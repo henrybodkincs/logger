14671f206312f6a98c87df0c6ab5e5d4462bfced
Henry Rivera <henryrivera.cs@gmail.com>
2022-04-19 16:28:12 -0400

    example updated to include with() example

--- example.py
+++ example.py
@@ -1,11 +1,23 @@
 from logger import Log
 
-my_log = Log(Name="My App")
-my_log.ok("Hello world!")
+def basic_example():
+    app_log = Log(Name="My App")
 
-x = 20
-if x > 21:
-    my_log.ok("X is greater than 21")
-else:
-    my_log.error("The math is wrong")
-my_log.warning("Done.")
+    app_log.ok("Starting script.")
+
+    someValue = True
+    if someValue:
+        app_log.ok("Value passed")
+    else:
+        app_log.error("Value failed")
+    app_log.info("Script is done.")
+
+
+def with_example():
+    with Log(Name="Admin Log", Level=3, FilePath="example.log") as admin_log:
+        admin_log.info("This will be stored to the example.log file")
+
+
+
+basic_example()
+with_example()
