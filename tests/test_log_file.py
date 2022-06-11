cb29dfda59fd3a2a497a09598611f1ba20b8f779
Henry <henryrivera.cs@gmail.com>
2022-06-11 11:40:16 -0400

    tests now checks if enabled/disabled will increase count + thatthe functions return None

--- tests/test_log_file.py
+++ tests/test_log_file.py
@@ -19,17 +19,7 @@ class Test(unittest.TestCase):
         self.assertEqual(log.file_path, None)
         self.assertEqual(log.enabled, True)
         self.assertEqual(log.level, 0)
-    
-    def test_set_log_file(self):
-        log = logger.Log(Name=log_name)
-        self.assertEqual(log.set_log_file(log_path), True)
-        self.assertEqual(log.file_path, log_path)
 
-    #test that saving strings into a specified file works as expected
-    """
-    def test_save_to_file(self):
-        log = logger.Log(Name=log_name)
-    """
 
     #Test if initializing Log() instance updates parameters as expected
     def test_disable_enable(self):
@@ -40,3 +30,55 @@ class Test(unittest.TestCase):
         log.enable()
         self.assertEqual(log.enabled, True)
 
+    #Test that disabling the logger will not output any statements or increase log_count
+    def test_disable_messages(self):
+        log = logger.Log(Name=log_name)
+        log.disable()
+        log.log_count = 0
+        self.assertEqual(log.ok("test"), None)
+        self.assertEqual(log.log_count, 0)
+        self.assertEqual(log.info("test"), None)
+        self.assertEqual(log.log_count, 0)
+        self.assertEqual(log.warning("test"), None)
+        self.assertEqual(log.log_count, 0)
+        self.assertEqual(log.error("test"), None)
+        self.assertEqual(log.log_count, 0)
+
+    
+    #Test that an enabled logger will try to output statements + increase log_count
+    def test_enabled_messages(self):
+        log = logger.Log(Name=log_name)
+        log.log_count = 0
+        self.assertEqual(log.ok("test"), None)
+        self.assertEqual(log.log_count, 1)
+        self.assertEqual(log.info("test"), None)
+        self.assertEqual(log.log_count, 2)
+        self.assertEqual(log.warning("test"), None)
+        self.assertEqual(log.log_count, 3)
+        self.assertEqual(log.error("test"), None)
+        self.assertEqual(log.log_count, 4)
+
+
+    def test_set_log_file(self):
+        log = logger.Log(Name=log_name)
+        self.assertEqual(log.set_log_file(log_path), True)
+        self.assertEqual(log.file_path, log_path)
+        log = logger.Log(Name=log_name, FilePath=log_path)
+        self.assertEqual(log.file_path, log_path)
+
+    """
+    #test that saving strings into a specified file works as expected
+    def test_save_to_file(self):
+        log = logger.Log(Name=log_name)
+    """
+    def test_set_log_level(self):
+        log = logger.Log(Name=log_name, Level=0)
+        log.disable()
+        self.assertEqual(log.level, 0)
+        self.assertEqual(log.set_log_level(1), False)
+        self.assertEqual(log.set_log_level(2), False)
+        self.assertEqual(log.set_log_level(None), False)
+        self.assertEqual(log.set_log_level("a"), False)
+        self.assertEqual(log.set_log_level(3), False)
+
+    
