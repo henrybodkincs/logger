4cef135adea12b6c31473b62d99894bcf248d7c4
Henry <henryrivera.cs@gmail.com>
2022-06-11 11:24:58 -0400

    updated logger to include Levels class for quick referencing + added 'set_log_level' with safety checks

--- logger.py
+++ logger.py
@@ -1,4 +1,5 @@
 import logging
+
 from datetime import datetime
 from os import path
 
@@ -16,6 +17,23 @@ class Styles:
     HEADER = "\033[95m"
     BOLD = "\033[1m"
     UNDERLINE = "\033[4m"
+
+class Headers:
+    """ Class for quickly accessing headers. """
+    INFO = "INFO"
+    OK = "OK"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+
+class Levels:
+    """ Quick access to logger.Logging() levels. """
+    NOTSET = 0
+    DEBUG = 10
+    INFO = 20
+    WARNING = 30
+    ERROR = 40
+    CRITICAL = 50
+
 
 class Log:
     """ Logging class for managing the formatting, storage and output of logs.
@@ -42,57 +60,40 @@ class Log:
 
     def __exit__(self, *args):
         self.info("Closing logger.")
-        #self.logger.close()
 
     def __init__(self, Name:str, Level:int=None, FilePath:str=None):
         self.log_count = 0
+        self.file_path = None
+        self.level = None
+
+        #flags for logging
+        self.save_ok = False
+        self.save_info = False
+        self.save_warning = False
+        self.save_error = False
 
         #Enable/Disable the logger
         self.enabled = True
         self.name = Name
         logging.basicConfig()
         self.logger = logging.getLogger(self.name)
-        #flags set to save specific logging-types to a file.
-        self.save_ok = False
-        self.save_info = False
-        self.save_warning = False
-        self.save_error = False
+        
+        self.log_file_worked = False
+        self.log_level_worked = False
 
-        if FilePath is None:
-            Level = 0 #set level to None since no filepath will be written to.
-            self.warning(Message=f"Log path was not specified/found. Log file will not be set for {self.name}.")
+        if FilePath is not None:
+            self.log_file_worked = self.set_log_file(FilePath)
         else:
-            if path.isfile(FilePath):
-                if self.set_log_file(FilePath):
-                    self.info(Message=f"Log file has been set.")
-                else:
-                    self.error(Message=f"Log file was unable to be configured properly. Please make sure the log file exists.")
-            else:
-                Level = 0 #do not try to save anything if the file is set.
-                self.error(Message=f"Specified file was not found. Log file will not be set for {self.name}")
+            self.log_file_worked = True
 
-        if Level is not None and (Level >= 0 and Level <= 3):
-            self.level = Level
-            if self.level == 1:
-                self.save_error = True
-            elif self.level == 2:
-                self.save_error = True
-                self.save_warning = True
-            elif self.level == 3:
-                self.save_ok = True
-                self.save_info = True
-                self.save_warning = True
-                self.save_error = True
-            #logger level always set to
-            #DEBUG since this class will handle whether a file goes to
-            #terminal or file.
-            self.logger.setLevel(level=logging.DEBUG)
-            self.info(Message=f"Log level set to: {self.level}")
+        if Level is None or isinstance(Level, int) is False:
+            Level = 0
+        self.log_level_worked = self.set_log_level(Level)
+
+        if self.log_level_worked and self.log_file_worked:
+            self.info(Message=f"Sucessfuly started logging instance.")
         else:
-            self.level = 0
-            self.error(Message=f"Log level out of bounds: {self.level}")
-
-        self.info(Message=f"Sucessfuly started logging instance for: {self.name}")
+            self.error(Message=f"Error occurred while initializing logging instance.")
 
     def disable(self):
         """ Disables all logging. """
@@ -109,18 +110,74 @@ class Log:
     def get_current_time(self):
         return datetime.now().time()
 
+    def set_log_level(self, Level:int):
+        """
+            Updates the logging level if it is in range and sets the flags for
+            logging into a file or terminal based off the new value.
+            Use this function instead of manually updating Log().level
+            as it will correctly update your flags for writing to files.
+        """
+        if not isinstance(Level, int):
+            self.error("Level must be an int() type. Please specify a valid log level.")
+            return False
+        if Level > 0 and self.file_path is None:
+            self.error("No FilePath has been specified for this level. Please use set_log_file(FilePath) to set a file path then call this function.")
+            return False
+
+        #executes exception when level is set to something other than an int/exists
+        if self.level is not None and self.level == Level:
+            self.warning(f"The specified level {Level} is already chosen. Please refer to the documentation/source code for more details on the available levels.")
+            return False
+        tmp = self.level
+        #reset the save flags 
+        self.save_ok = False
+        self.save_info = False
+        self.save_warning = False
+        self.save_error = False
+
+        if Level >= 0 and Level <= 3:
+            self.level = Level
+            if self.level == 1:
+                self.save_error = True
+            elif self.level == 2:
+                self.save_error = True
+                self.save_warning = True
+            elif self.level == 3:
+                self.save_ok = True
+                self.save_info = True
+                self.save_warning = True
+                self.save_error = True
+
+            #logger level will always be set to DEBUG since
+            #the class itself will handle whether a file goes to terminal or file.
+            self.logger.setLevel(level=logging.DEBUG)
+            self.info(f"Log level successfully set to {self.level}.")
+            return True
+        else:
+            self.warning(f"Specified level {Level} is not a valid range. Please refer to the documentation for more details.")
+            return False
 
     def set_log_file(self, FilePath:str):
-        if path.isfile(FilePath):
-            file_handler = logging.FileHandler(
-                    filename=FilePath,
-                    mode="a",
-                    encoding="utf-8"
-                    )
-            formatter = logging.Formatter("%(message)s")
-            file_handler.setFormatter(formatter)
-            self.logger.addHandler(file_handler)
-            return True
+        try:
+            if FilePath is None:
+                self.warning(f"No file path has been specified for this log.")
+                return False
+            if path.isfile(FilePath):
+                file_handler = logging.FileHandler(
+                        filename=FilePath,
+                        mode="a",
+                        encoding="utf-8"
+                        )
+                formatter = logging.Formatter("%(message)s")
+                file_handler.setFormatter(formatter)
+                self.logger.addHandler(file_handler)
+                self.file_path = FilePath
+                self.ok(f"Successfully set file path for logger to: {FilePath}")
+                return True
+            else:
+                self.error(f"The specified file path is not an existing file. Please create it to set the new log path.")
+        except Exception as e:
+            self.error(f"An error occurred while setting the log file: {e}")
         return False
 
 
@@ -157,54 +214,51 @@ class Log:
         self.log_count += 1
 
     def info_to_file(self, Message:str):
-        """ TODO """
-        level = 10
+        level = Levels.DEBUG
         header = "INFO"
         self.log_to_file(Header=header, Level=level, Message=Message)
 
 
     def ok_to_file(self, Message:str):
-        """ TODO """
-        level = 10
-        header = "OK"
-        #header = "OK"
+        level = Levels.DEBUG
+        header = Headers.OK
         self.log_to_file(Header=header, Level=level, Message=Message)
     
     def warning_to_file(self, Message:str):
-        header = "WARNING"
-        level = 10
+        header = Headers.WARNING
+        level = Levels.DEBUG
         self.log_to_file(Header=header, Level=level, Message=Message)
 
 
     def error_to_file(self, Message:str):
-        header = "ERROR"
-        level = 10
+        header = Headers.ERROR
+        level = Levels.DEBUG
         self.log_to_file(Header=header, Level=level, Message=Message)
 
 
     def warning_to_terminal(self, Message:str):
-        header = "WARNING"
+        header = Headers.WARNING
         color = Colors.WARNING
         style = Styles.BOLD
         self.log_to_terminal(Message=Message, Header=header, Color=color, Style=style)
 
 
     def error_to_terminal(self, Message:str):
-        header = "ERROR"
+        header = Headers.ERROR
         color = Colors.ERROR
         style = Styles.BOLD
         self.log_to_terminal(Message=Message, Header=header, Color=color, Style=style)
 
 
     def ok_to_terminal(self, Message:str):
-        header = "OK"
+        header = Headers.OK
         color = Colors.OKBLUE
         style = Styles.BOLD
         self.log_to_terminal(Message=Message, Header=header, Color=color, Style=style)
 
 
     def info_to_terminal(self, Message:str):
-        header = "INFO"
+        header = Headers.INFO
         color = Colors.OKCYAN
         style = Styles.BOLD
         self.log_to_terminal(Message=Message, Header=header, Color=color, Style=style)
