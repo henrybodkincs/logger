4935a095e7f9750a1170eca484835a5a9a09e739
Henry Rivera <henryrivera.cs@gmail.com>
2022-06-13 12:14:40 -0400

    apply black linting + added CRITICAL flag at level 1

--- logger/logger.py
+++ logger/logger.py
@@ -3,57 +3,54 @@ import logging
 from datetime import datetime
 from os import path
 
+
 class Colors:
-    """ Class to be used for setting logging colors. """
-    WARNING = "\033[93m"
-    ERROR = "\033[91m"
-    OKBLUE = "\033[94m"
-    OKCYAN = "\033[96m"
-    OKGREEN = "\033[92m"
-    ENDC  = "\033[0m"
+    """Class to be used for setting logging colors."""
+
+    YELLOW = "\033[93m"
+    RED = "\033[91m"
+    BLUE = "\033[94m"
+    CYAN = "\033[96m"
+    GREEN = "\033[92m"
+    ENDC = "\033[0m"
+
 
 class Styles:
-    """ Class to be used for setting different font types. """
+    """Class to be used for setting different font types."""
+
     HEADER = "\033[95m"
     BOLD = "\033[1m"
     UNDERLINE = "\033[4m"
 
+
 class Headers:
-    """ Class for quickly accessing headers. """
+    """Class for quickly accessing headers."""
+
     INFO = "INFO"
     OK = "OK"
     WARNING = "WARNING"
     ERROR = "ERROR"
-
-class Levels:
-    """ Quick access to logger.Logging() levels. """
-    NOTSET = 0
-    DEBUG = 10
-    INFO = 20
-    WARNING = 30
-    ERROR = 40
-    CRITICAL = 50
+    CRITICAL = "CRITICAL"
 
 
 class Log:
-    """ Logging class for managing the formatting, storage and output of logs.
+    """Logging class for managing the formatting, storage and output of logs.
 
-    Logging Types:
-        [ERROR] = raised for issues that return application-breaking responses that occured while handling some request
-        [WARNING] = raised for unexpected actions or processing some request
-        [INFO] = raised for notifying the admins of something
-        [OK] = raised when a process executed as expected (good for tests!)
+    Logging Types: CRITICAL, ERROR, WARNING, INFO, OK
 
     Logging Levels:
         Level 0:
             Logs everything to the Terminal.
         Level 1:
-            Logs ERROR to Files. Logs WARNING/INFO/OK messages to Terminal.
+            Logs CRITICAL to files. Logs ERROR/WARNING/INFO/OK messages to Terminal.
         Level 2:
-            Logs ERROR/WARNING to Files. - Logs INFO/OK to Terminal.
+            Logs ERROR/CRITICAL to files. Logs WARNING/INFO/OK to Terminal.
         Level 3:
+            Logs ERROR/CRITICAL/WARNING to files.  Logs INFO/OK to Terminal
+        Level 4:
             Logs everything to Files.
     """
+
     def __enter__(self):
         self.info("Opening logger.")
         return self
@@ -61,223 +58,245 @@ class Log:
     def __exit__(self, *args):
         self.info("Closing logger.")
 
-    def __init__(self, Name:str, Level:int=None, FilePath:str=None):
+    def __init__(self, Name: str, Level: int = None, FilePath: str = None):
+        self.name = Name
+        self.logger = logging.getLogger(self.name)
         self.log_count = 0
         self.level = 0
+        self.enabled = True
         self.file_path = None
 
-        #flags for logging
+        # flags for logging
         self.save_ok = False
         self.save_info = False
         self.save_warning = False
         self.save_error = False
+        self.save_critical = False
 
-        #Enable/Disable the logger
-        self.enabled = True
-        self.name = Name
-        #logging.basicConfig()
-        self.logger = logging.getLogger(self.name)
-        
-        self.log_file_worked = False
-        self.log_level_worked = False
-
-        self.log_file_set = self.set_log_file(FilePath)
-        self.log_level_set = self.set_log_level(Level)
+        self.set_log_file(FilePath)
+        self.set_log_level(Level)
 
     def disable(self):
-        """ Disables all logging. """
+        """Disables all logging."""
         self.enabled = False
-        self.logger.disabled = True #just in case
-
+        self.logger.disabled = True
 
     def enable(self):
-        """ Enables all logging """
+        """Enables all logging"""
         self.enabled = True
-        self.logger.disabled = False #just in case
-
+        self.logger.disabled = False
 
     def get_current_time(self):
         return datetime.today()
 
-
-    def set_log_level(self, Level:int):
+    def set_log_level(self, Level: int):
         """
-            Updates the logging level if it is in range and sets the flags for
-            logging into a file or terminal based off the new value.
-            Use this function instead of manually updating Log().level
-            as it will correctly update your flags for writing to files.
+        Updates the logging level if it is in range and sets the flags for
+        logging into a file or terminal based off the new value.
+        Use this function instead of manually updating Log().level
+        as it will correctly update your flags for writing to files.
         """
         if not isinstance(Level, int):
             return False
         if Level > 0 and self.file_path is None:
-            self.error("No FilePath has been specified for this log. Please use set_log_file(FilePath) to set a file path then call this function.")
+            self.error(
+                "No FilePath has been specified for this log. Please use set_log_file(FilePath) to set a file path then call this function."
+            )
 
-        if Level >= 0 and Level <= 3:
-            #reset the save flags 
+        if Level >= 0 and Level <= 4:
+            # reset the save flags
             self.save_ok = False
             self.save_info = False
             self.save_warning = False
             self.save_error = False
+            self.save_critical = False
             self.level = Level
             if self.level == 1:
-                self.save_error = True
+                self.save_critical = True
             elif self.level == 2:
                 self.save_error = True
-                self.save_warning = True
+                self.save_critical = True
             elif self.level == 3:
-                self.save_ok = True
-                self.save_info = True
                 self.save_warning = True
                 self.save_error = True
+                self.save_critical = True
+            elif self.level == 4:
+                self.save_warning = True
+                self.save_error = True
+                self.save_critical = True
+                self.save_ok = True
+                self.save_info = True
 
-            #logger level will always be set to DEBUG since
-            #the class itself will handle whether a file goes to terminal or file.
+            # logger level will always be set to DEBUG since
+            # the class itself will handle whether a file goes to terminal or file.
             self.info(f"Log level set to {self.level}.")
             return True
         else:
-            self.warning(f"Specified level {Level} is not a valid range. Please refer to the documentation for more details.")
+            self.warning(
+                f"Specified level {Level} is not a valid range. Please refer to the documentation for more details."
+            )
             return False
 
-    def set_log_file(self, FilePath:str):
+    def set_log_file(self, FilePath: str):
         try:
             if FilePath is None:
                 self.warning(f"No file path has been specified for this log.")
             elif path.isfile(FilePath):
                 file_handler = logging.FileHandler(
-                        filename=FilePath,
-                        mode="a",
-                        encoding="utf-8"
-                        )
+                    filename=FilePath, mode="a", encoding="utf-8"
+                )
                 self.logger.addHandler(file_handler)
                 self.file_path = FilePath
                 self.logger.setLevel(level=logging.DEBUG)
                 self.ok(f"Pointing to new file path for logger: {FilePath}")
                 return True
             else:
-                self.error(f"The specified file path is not an existing file. Please create it to set the new log path.")
+                self.error(
+                    f"The specified file path is not an existing file. Please create it to set the new log path."
+                )
         except Exception as e:
             self.error(f"An error occurred while setting the log file: {e}")
         return False
 
-
-    def log_to_terminal(self, Message:str, Header:str=None, Color:str=None, Style:str=None):
+    def log_to_terminal(
+        self, Message: str, Header: str = None, Color: str = None, Style: str = None
+    ):
         current_time = self.get_current_time()
         if Header is None:
             Header = "LOG"
         if Color is None and Style is None:
-            print(f"[{Header}] [{self.name} - {self.log_count}]: {current_time} - {Message}")
+            print(
+                f"[{Header}] [{self.name} - {self.log_count}]: {current_time} - {Message}"
+            )
         else:
-        #pass formatting appropiately:
+            # pass formatting appropiately:
             if Color is not None and Style is not None:
-                #color and style
-                print(f"[{current_time}] [{Color}{Style}{Header}{Colors.ENDC}] [{self.name}] [{self.log_count}] - {Message}")
+                # color and style
+                print(
+                    f"[{current_time}] [{Color}{Style}{Header}{Colors.ENDC}] [{self.name}] [{self.log_count}] - {Message}"
+                )
             elif Color is not None and Style is None:
-                #only color
-                print(f"[{current_time}] [{Color}{Header}{Colors.ENDC}] [{self.name}] [{self.log_count}] - {Message}")
+                # only color
+                print(
+                    f"[{current_time}] [{Color}{Header}{Colors.ENDC}] [{self.name}] [{self.log_count}] - {Message}"
+                )
             else:
-                #only style
-                print(f"[{current_time}] [{Style}{Header}{Colors.ENDC}] [{self.name}] [{self.log_count}] - {Message}")
+                # only style
+                print(
+                    f"[{current_time}] [{Style}{Header}{Colors.ENDC}] [{self.name}] [{self.log_count}] - {Message}"
+                )
 
         self.log_count += 1
 
-
-    def log_to_file(self, Header:str, Level:int, Message:str):
-        """ Logs the specific message into the log file(assuming the file path was set and the logger is enabled."""
+    def log_to_file(self, Header: str, Level: int, Message: str):
+        """Logs the specific message into the log file(assuming the file path was set and the logger is enabled."""
         current_time = self.get_current_time()
         if Header is None:
             Header = "LOG"
-        formatted_msg = f"[{current_time}] [{Header}] [{self.name}] [{self.log_count}] - {Message}"
+        formatted_msg = (
+            f"[{current_time}] [{Header}] [{self.name}] [{self.log_count}] - {Message}"
+        )
         self.logger.propagate = False
         self.logger.log(level=logging.CRITICAL, msg=formatted_msg)
         self.logger.propagate = True
         self.log_count += 1
 
-    def info_to_file(self, Message:str):
-        level = Levels.DEBUG
+    def info_to_file(self, Message: str):
+        level = logging.DEBUG
         header = "INFO"
         self.log_to_file(Header=header, Level=level, Message=Message)
 
-
-    def ok_to_file(self, Message:str):
-        level = Levels.DEBUG
+    def ok_to_file(self, Message: str):
+        level = Logging.DEBUG
         header = Headers.OK
         self.log_to_file(Header=header, Level=level, Message=Message)
-    
-    def warning_to_file(self, Message:str):
+
+    def warning_to_file(self, Message: str):
         header = Headers.WARNING
-        level = Levels.DEBUG
+        level = logging.DEBUG
         self.log_to_file(Header=header, Level=level, Message=Message)
 
-
-    def error_to_file(self, Message:str):
+    def error_to_file(self, Message: str):
         header = Headers.ERROR
-        level = Levels.DEBUG
+        level = logging.DEBUG
         self.log_to_file(Header=header, Level=level, Message=Message)
 
+    def critical_to_file(self, Message: str):
+        header = Headers.CRITICAL
+        level = logging.DEBUG
+        self.log_to_file(Header=header, Level=level, Message=Message)
 
-    def warning_to_terminal(self, Message:str):
+    def warning_to_terminal(self, Message: str):
         header = Headers.WARNING
-        color = Colors.WARNING
+        color = Colors.YELLOW
         style = Styles.BOLD
         self.log_to_terminal(Message=Message, Header=header, Color=color, Style=style)
 
-
-    def error_to_terminal(self, Message:str):
+    def error_to_terminal(self, Message: str):
         header = Headers.ERROR
-        color = Colors.ERROR
+        color = Colors.RED
         style = Styles.BOLD
         self.log_to_terminal(Message=Message, Header=header, Color=color, Style=style)
 
+    def critical_to_terminal(self, Message: str):
+        header = Headers.CRITICAL
+        color = Colors.RED
+        style = Styles.BOLD
+        self.log_to_terminal(Message=Message, Header=header, Color=color, Style=style)
 
-    def ok_to_terminal(self, Message:str):
+    def ok_to_terminal(self, Message: str):
         header = Headers.OK
-        color = Colors.OKBLUE
+        color = Colors.BLUE
         style = Styles.BOLD
         self.log_to_terminal(Message=Message, Header=header, Color=color, Style=style)
 
-
-    def info_to_terminal(self, Message:str):
+    def info_to_terminal(self, Message: str):
         header = Headers.INFO
-        color = Colors.OKCYAN
+        color = Colors.CYAN
         style = Styles.BOLD
         self.log_to_terminal(Message=Message, Header=header, Color=color, Style=style)
 
-
-    def ok(self, Message:str):
-        """ Logs [OK] messages to either the terminal or file depending on self.level """
+    def ok(self, Message: str):
+        """Logs [OK] messages to either the terminal or file depending on self.level"""
         if not self.enabled:
-            return
+            return 1
         if self.save_ok:
             self.ok_to_file(Message)
         else:
             self.ok_to_terminal(Message)
 
-
-    def error(self, Message:str):
-        """ Logs [ERROR] messages to either the terminal or file depending on self.level """
+    def error(self, Message: str):
+        """Logs [ERROR] messages to either the terminal or file depending on self.level"""
         if not self.enabled:
-            return
+            return 1
         if self.save_error:
             self.error_to_file(Message)
         else:
             self.error_to_terminal(Message)
 
-
-    def info(self, Message:str):
-        """ Logs [INFO] messages to either the terminal or file depending on self.level """
+    def info(self, Message: str):
+        """Logs [INFO] messages to either the terminal or file depending on self.level"""
         if not self.enabled:
-            return
+            return 1
         if self.save_info:
             self.info_to_file(Message)
         else:
             self.info_to_terminal(Message)
 
-
-    def warning(self, Message:str):
-        """ Logs [WARNING] messages to either the terminal or file depending on self.level """
+    def warning(self, Message: str):
+        """Logs [WARNING] messages to either the terminal or file depending on self.level"""
         if not self.enabled:
-            return
+            return 1
         if self.save_warning:
             self.warning_to_file(Message)
         else:
             self.warning_to_terminal(Message)
+
+    def critical(self, Message: str):
+        """Logs [CRITICAL] messages to either the terminal or file depending on self.level"""
+        if not self.enabled:
+            return 1
+        if self.save_critical:
+            self.critical_to_file(Message)
+        else:
+            self.critical_to_terminal(Message)
--- tests/test_log_file.py
+++ tests/test_log_file.py
@@ -6,20 +6,19 @@ from logger import logger
 log_path = "tests/test_log.log"
 log_name = "Test Logger"
 
+
 class TestLogging:
     def test_default_init(self):
         log = logger.Log(Name=log_name)
+        assert log.level == 0
         assert log.name == log_name
+        assert log.save_critical is False
         assert log.save_error is False
         assert log.save_warning is False
         assert log.save_ok is False
         assert log.save_info is False
         assert log.file_path is None
-        assert log.log_file_set is False
-        assert log.log_level_set is False
         assert log.enabled is True
-        assert log.level == 0
-
 
     def test_disable_enable(self):
         log = logger.Log(Name=log_name)
@@ -29,21 +28,21 @@ class TestLogging:
         log.enable()
         assert log.enabled is True
 
-
     def test_disabled_messages(self):
         log = logger.Log(Name=log_name)
         log.disable()
         log.log_count = 0
-        assert log.ok("test") is None
+        assert log.ok("test") is 1
         assert log.log_count == 0
-        assert log.info("test") is None
+        assert log.info("test") is 1
         assert log.log_count == 0
-        assert log.warning("test") is None
+        assert log.warning("test") is 1
         assert log.log_count == 0
-        assert log.error("test") is None
+        assert log.error("test") is 1
+        assert log.log_count == 0
+        assert log.critical("test") is 1
         assert log.log_count == 0
 
-    
     def test_enabled_messages(self):
         log = logger.Log(Name=log_name)
         log.log_count = 0
@@ -55,7 +54,8 @@ class TestLogging:
         assert log.log_count == 3
         assert log.error("Testing ERROR") is None
         assert log.log_count == 4
-
+        assert log.critical("Testing ERROR") is None
+        assert log.log_count == 5
 
     def test_set_log_file(self, caplog):
         other_log = logger.Log(Name=log_name, FilePath=log_path)
@@ -63,7 +63,6 @@ class TestLogging:
         assert other_log.file_path == log_path
         assert log.set_log_file(log_path) is True
         assert log.file_path == log_path
-
 
     """
     def test_save_to_file(self, caplog):
@@ -77,42 +76,54 @@ class TestLogging:
 
     def test_set_log_level(self):
         log = logger.Log(Name=log_name, Level=0)
-        #test 0 flags
+        # test 0 flags
         assert log.level == 0
-        assert log.save_error is False
-        assert log.save_ok is False
         assert log.save_info is False
+        assert log.save_critical is False
+        assert log.save_ok is False
+        assert log.save_error is False
         assert log.save_warning is False
-        #test 1 flags
+        # test 1 flags
         assert log.set_log_level(1) is True
         assert log.level == 1
-        assert log.save_error is True
+        assert log.save_critical is True
+        assert log.save_error is False
+        assert log.save_warning is False
         assert log.save_ok is False
         assert log.save_info is False
-        assert log.save_warning is False
-        #test 2 flags
+        # test 2 flags
         assert log.set_log_level(2) is True
         assert log.level == 2
+        assert log.save_critical is True
         assert log.save_error is True
+        assert log.save_warning is False
         assert log.save_ok is False
         assert log.save_info is False
-        assert log.save_warning is True
-        #test 3 flags
+        # test 3 flags
         assert log.set_log_level(3) is True
         assert log.level == 3
+        assert log.save_critical is True
         assert log.save_error is True
+        assert log.save_warning is True
+        assert log.save_ok is False
+        assert log.save_info is False
+        # test 4 flags
+        assert log.set_log_level(4) is True
+        assert log.level == 4
+        assert log.save_critical is True
+        assert log.save_error is True
+        assert log.save_warning is True
         assert log.save_ok is True
         assert log.save_info is True
-        assert log.save_warning is True
-        #test that flags/levels aren't reset on invalid inputs
+        # test that flags/levels aren't reset on invalid inputs
         assert log.set_log_level(None) is False
-        assert log.level == 3
+        assert log.level == 4
         assert log.set_log_level("a") is False
-        assert log.level == 3
+        assert log.level == 4
         assert log.set_log_level(33) is False
-        assert log.level == 3
+        assert log.level == 4
+        assert log.save_critical is True
         assert log.save_error is True
         assert log.save_ok is True
         assert log.save_info is True
         assert log.save_warning is True
-
